5267fef899b82c339e7b082b1bc194bda7fc92cb
Dylan Yarbrough <129237422+DillDogG@users.noreply.github.com>
2025-08-09 00:30:10 -0600

    Added jumping and gravity
    Jumping still needs to be changed to only work on the ground

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,12 +2,18 @@ extends CharacterBody2D
 signal hit
 
 @export var move_speed = 300
+@export var jump_force = 100
 
 func read_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * move_speed
+	var input_direction = Input.get_axis("left", "right")
+	velocity.x =input_direction * move_speed
 	
 func _physics_process(delta):
+	velocity.y += (get_gravity().y * delta) / 3
+	if Input.is_action_pressed("jump"):
+		velocity.y = -jump_force
+	if velocity.y > get_gravity().y * 0.01667 * 10:
+		velocity.y = get_gravity().y * 0.01667 * 10
 	read_input()
 	move_and_slide()
 
