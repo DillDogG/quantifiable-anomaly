b4e84326ab496f6437491705ec8e5a84a204e56b
Raxus4444 <aritaylor1@gmail.com>
2025-08-08 22:48:00 -0600

    Add player script
    Copied a player script from my previous testing. Basic skeleton to change and build from.

---
+++ Scripts/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+signal hit
+
+@export var move_speed = 300
+
+func read_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * move_speed
+	
+func _physics_process(delta):
+	read_input()
+	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
