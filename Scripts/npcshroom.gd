9439816907fcabd0eb9e2a181e65aba6b790af2f
Raxus4444 <aritaylor1@gmail.com>
2025-08-09 23:41:23 -0600

    Lots
    added evil jam to spritesheet, created code and scenes for all mushroom npcs, created and coded moving platforms, edited level 1.

---
+++ Scripts/npcshroom.gd
@@ -0,0 +1,28 @@
+extends RigidBody2D
+
+@export var move_speed = -50
+var timer: float = 0.0
+var timerWalk: float = 0.0
+@export var timerSet = 3
+@export var timerSetWalk = 1
+var animator
+
+func _ready():
+	animator = $AnimatedSprite2D
+	animator.play("idle")
+	
+
+func _physics_process(delta):
+	timer -= delta
+	if timer <= 0:
+		linear_velocity.x = move_speed
+		animator.play("walk")
+		timerWalk -= delta
+		if timerWalk <= 0:
+			timerWalk = timerSetWalk
+			linear_velocity.x = 0
+			animator.play("idle")
+			move_speed *= -1
+			animator.flip_h = move_speed < 0
+			print(move_speed)
+			timer = timerSet
