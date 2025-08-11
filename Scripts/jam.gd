9feb1a55797c8441a7d8f0c963f3f4cfe579c230
Raxus4444 <aritaylor1@gmail.com>
2025-08-10 23:45:42 -0600

    deleting centipede
    cute centipede now deletes properly in level 4

--- Scripts/jam.gd
+++ Scripts/jam.gd
@@ -8,5 +8,10 @@ func _ready():
 	animator.play("default")
 
 
-func _on_body_entered(body: Player) -> void:
-	get_tree().change_scene_to_file(levelPath)
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file(levelPath)
+	elif body.is_in_group("cute"):
+		body.queue_free()
+		await get_tree().create_timer(2).timeout
+		queue_free()
